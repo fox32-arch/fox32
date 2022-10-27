fb9f8ccf930b2926f2e82be107cf13bf67536dda
Ry <ryanst755@gmail.com>
2022-10-26 18:02:30 -0700

    Remove Rust emulator

--- build.rs
+++
@@ -1,9 +0,0 @@
-use anyhow::Result;
-use vergen::{Config, ShaKind, vergen};
-
-fn main() -> Result<()> {
-    let mut config = Config::default();
-    *config.git_mut().sha_kind_mut() = ShaKind::Short;
-    *config.git_mut().skip_if_error_mut() = true;
-    vergen(config)
-}
